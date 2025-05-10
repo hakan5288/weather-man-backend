222924c5c2f8f6b297ea4271c4f9d13c798c4dbf
Ubuntu <ubuntu@ip-172-31-21-33.eu-west-2.compute.internal>
2025-05-10 16:41:36 +0000

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],

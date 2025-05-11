473f5879a4cfffc42fd91f2a75c3ba53f08a668c
hakan5288 <hassanxh001@gmail.com>
2025-05-11 12:17:56 +0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! this is a simple test deployment for github for ci/cd pipeline';
+    return 'Hello World! this is a simple test deployment for github for ci/cd pipeline please run please ';
   }
 }
